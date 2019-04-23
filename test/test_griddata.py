cc2c0c3c054b12790c2e7015a7ad4b1f5412720e
Jerahmie Radder <jerahmie@cmrr.umn.edu>
2019-04-23 16:03:50 -0500

    Added unittest framework for griddata tests.

---
+++ test/test_griddata.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Test griddata extractor for CST.
+"""
+
+import unittest
+
+class TestGridData(unittest.TestCase):
+    """Tests for griddata extraction from CST project.
+    """
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def tearDownClass(self):
+        pass
+
+if __name__ == "__main__":
+    print("Testing...")
+    unittest.main()
+    
