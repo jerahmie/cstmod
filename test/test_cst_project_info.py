aff3a73bd8928a86241b78e3d6880366c0a19d98
Jerahmie Radder <radd0012@umn.edu>
2019-04-24 13:59:52 -0500

    added test files for cstmod interface

---
+++ test/test_cst_project_info.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Test the ability to extract cst project information using CSTResultReader.dll
+"""
+
+import unittest
+
+class TestProjectInfo(unittest.TestCase):
+    """Tests CSTProject class.
+    """
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
