0322bf3d1e84b9a8ab6636a97f91d3d91b7cca45
Jerahmie Radder <jerahmie@gmail.com>
2023-01-27 17:16:19 -0600

    prepping for export file comparison

--- test/test_validate_vopgen_cst.py
+++ test/test_validate_vopgen_cst.py
@@ -1,7 +1,7 @@
 """
 Unit tests for validation of fields exported from cst with applied shims.
 """
-
+import os
 import unittest
 from cstmod.field_reader import FieldReaderH5
 
@@ -16,15 +16,30 @@ class TestValidateVopgenCST(unittest.TestCase):
         pass
 
     def setUp(self):
-        pass
+        self.test_data_directory = os.path.join(os.getcwd(), r'..',
+                                                r'test_data')
+        self.nchannels = 8    # number of channels in array
+        self.frequency = 297  # frequency (MHz)
+        self.field_file_names = ["e-field (f=" + str(self.frequency) +
+                                ") [AC" + str(i+1) + "].h5"
+                                for i in range(self.nchannels)]
 
     @unittest.skip
     def test_the_tests(self):
+        """
+        Simple test to check the unittest setup.  Initially fails, then
+        passes, then is skipped.
+        """
         self.assertTrue(True)
 
     def test_per_channel(self):
-        self.assertTrue(True)
-        
+        """
+        Compare single channel fields exported from CST with Vopgen fields.
+        """
+        for field_file in self.field_file_names:
+            efield_file = os.path.join(self.test_data_directory, field_file)
+            self.assertTrue(os.path.exists(efield_file))
+
     def tearDown(self):
         pass
 
